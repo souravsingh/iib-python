d762d55e3e6f6a25e860079d0f9b07cddad7c799
Sourav Singh <souravsingh@users.noreply.github.com>
2016-02-23 00:43:49 +0530

    Update iib.py

--- src/iib.py
+++ src/iib.py
@@ -3,4 +3,14 @@ import json
 import os
 import getpass
 
-url = 'http://www.reddit.com/r/InternetisBeautiful.json' 
+url = 'http://www.reddit.com/r/InternetisBeautiful/top.json' 
+
+def main():
+	
+	obj = urllib2.urlopen(url)
+
+	
+	data = json.load(obj)
+
+	
+	count = 0
